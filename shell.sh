e39033cf45c5d434cae20b8ba6fcae2512ea5514
Ganeshdevops93 <ganeshkadiri496@mail.com>
2021-05-12 17:56:44 +0530

    aa fiel shell f

--- shell.sh
+++ shell.sh
@@ -8,4 +8,8 @@ echo  -e "hai\t\tvali"
 echo -e "well\nvali"
 echo -e "\e[31mWARNING!! THere is an error"
 
+echo -e "\e[32mWARNING!! hai "
+echo -e "\e[33mWARNING!! error"
+echo -e "\e[34mWARNING!! THere is an error"
+echo -e "\e[35mWARNING!! THere is an error"
 
